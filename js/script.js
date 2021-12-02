67cdd3e7b2d69f7664455a27d1aaf8d0f7d66d7a
quaco9204 <quaco@naver.com>
2021-12-03 06:20:22 +0900

    portfolio_ver.08

---
+++ js/script.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {});
+
+window.onload = function () {
+
+    var bar_ps = new ProgressBar.Circle(skill_ps, {
+        strokeWidth: 6,
+        easing: 'easeInOut',
+        duration: 1400,
+        color: '#d8bc34',
+        trailColor: '#eee',
+        trailWidth: 12,
+        svgStyle: null
+    });
+    bar_ps.animate(0.5);  // Number from 0.0 to 1.0
+
+    var bar_ps2 = new ProgressBar.Circle(skill_ps2, {
+        strokeWidth: 6,
+        easing: 'easeInOut',
+        duration: 1400,
+        color: '#d8bc34',
+        trailColor: '#eee',
+        trailWidth: 12,
+        svgStyle: null
+    });
+    bar_ps2.animate(0.5);  // Number from 0.0 to 1.0
+
+    var bar_ps3 = new ProgressBar.Circle(skill_ps3, {
+        strokeWidth: 6,
+        easing: 'easeInOut',
+        duration: 1400,
+        color: '#d8bc34',
+        trailColor: '#eee',
+        trailWidth: 12,
+        svgStyle: null
+    });
+    bar_ps3.animate(0.5);  // Number from 0.0 to 1.0
+
+    var bar_ps4 = new ProgressBar.Circle(skill_ps4, {
+        strokeWidth: 6,
+        easing: 'easeInOut',
+        duration: 1400,
+        color: '#d8bc34',
+        trailColor: '#eee',
+        trailWidth: 12,
+        svgStyle: null
+    });
+    bar_ps4.animate(0.5);  // Number from 0.0 to 1.0
+
+    var bar_ps5 = new ProgressBar.Circle(skill_ps5, {
+        strokeWidth: 6,
+        easing: 'easeInOut',
+        duration: 1400,
+        color: '#d8bc34',
+        trailColor: '#eee',
+        trailWidth: 12,
+        svgStyle: null
+    });
+    bar_ps5.animate(0.5);  // Number from 0.0 to 1.0
+
+}

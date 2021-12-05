5ead152b97e98deba52a2b1bafc31f4744ebfa5d
quaco9204 <quaco@naver.com>
2021-12-06 01:46:30 +0900

    portfolio_ver1.0

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ window.onload = function () {
     });
     bar_ps.animate(0.5);  // Number from 0.0 to 1.0
 
-    var bar_ps2 = new ProgressBar.Circle(skill_ps2, {
+    var bar_il = new ProgressBar.Circle(skill_il, {
         strokeWidth: 6,
         easing: 'easeInOut',
         duration: 1400,
@@ -22,9 +22,9 @@ window.onload = function () {
         trailWidth: 12,
         svgStyle: null
     });
-    bar_ps2.animate(0.5);  // Number from 0.0 to 1.0
+    bar_il.animate(0.25);  // Number from 0.0 to 1.0
 
-    var bar_ps3 = new ProgressBar.Circle(skill_ps3, {
+    var bar_html = new ProgressBar.Circle(skill_html, {
         strokeWidth: 6,
         easing: 'easeInOut',
         duration: 1400,
@@ -33,9 +33,9 @@ window.onload = function () {
         trailWidth: 12,
         svgStyle: null
     });
-    bar_ps3.animate(0.5);  // Number from 0.0 to 1.0
+    bar_html.animate(0.75);  // Number from 0.0 to 1.0
 
-    var bar_ps4 = new ProgressBar.Circle(skill_ps4, {
+    var bar_js = new ProgressBar.Circle(skill_js, {
         strokeWidth: 6,
         easing: 'easeInOut',
         duration: 1400,
@@ -44,9 +44,9 @@ window.onload = function () {
         trailWidth: 12,
         svgStyle: null
     });
-    bar_ps4.animate(0.5);  // Number from 0.0 to 1.0
+    bar_js.animate(0.65);  // Number from 0.0 to 1.0
 
-    var bar_ps5 = new ProgressBar.Circle(skill_ps5, {
+    var bar_vue = new ProgressBar.Circle(skill_vue, {
         strokeWidth: 6,
         easing: 'easeInOut',
         duration: 1400,
@@ -55,25 +55,46 @@ window.onload = function () {
         trailWidth: 12,
         svgStyle: null
     });
-    bar_ps5.animate(0.5);  // Number from 0.0 to 1.0
+    bar_vue.animate(0.4);  // Number from 0.0 to 1.0
 
+    
+    // let options = {};
+    // if ( $(".swiper-container .swiper-slide").length < 3 ){
+    //     options = {            
+    //         loop: false
+    //     }
+    // } else {
+    //     options = {
+    //         loop: true,
+    //         autoplay: false,
+    //     }
+    // }
 
-    new Swiper('.sw-main', {
+    let sw_main = new Swiper('.sw-main', {
         loop: true,
+        slidesPerView: 3,        
+
         autoplay: {
             delay: 3000,
             disableOnInteraction: false,
         },
-        allowTouchMove: false,
-        effect: 'fade',
-        fadeEffect: {
-            crossFade: true,
-        },
-        speed: 1000,
-        navigation: {
-            prevEl: '.sw-main-prev',
-            nextEl: '.sw-main-next',
-        },
+        
+        // navigation: {
+        //     prevEl: '.sw-main-prev',
+        //     nextEl: '.sw-main-next',
+        // },
     });
 
+    let sw_sub = new Swiper('.sw-sub', {
+        loop: false,
+        slidesPerView: 3,        
+
+        autoplay: false
+        
+        // navigation: {
+        //     prevEl: '.sw-main-prev',
+        //     nextEl: '.sw-main-next',
+        // },
+    });
+    
 }

d481305de35661de91dc75cd2ff826e859f10630
quaco9204 <quaco@naver.com>
2021-12-13 15:07:23 +0900

    portfolio_ver1.36

--- js/animatecustom.js
+++ js/animatecustom.js
@@ -1,49 +1,6 @@
 $(document).ready(function () {
+
+   
     
     
 });
-
-window.onload = function () {
-
-    // section_visual animation
-    let mini_01 = $('.mini-01');
-    let mini_box_01 = $('.mini-01 .mini-box-01');
-    let mini_02 = $('.mini-02');
-    let mini_box_02 = $('.mini-02 .mini-box-02');
-    let mini_03 = $('.mini-03');
-    let mini_box_03 = $('.mini-03 .mini-box-03');
-    let mini_04 = $('.mini-04');
-    let mini_box_04 = $('.mini-04 .mini-box-04');
-    let mini_05 = $('.mini-05');
-    let mini_box_05 = $('.mini-05 .mini-box-05');
-    
-    mini_01.effect( "bounce", {times:1}, 300,'ease out bounce');
-    mini_box_01.css({opacity: 1})
-    
-    setTimeout(
-        function(){
-            mini_02.effect( "bounce", {times:1}, 300,'ease out bounce');
-            mini_box_02.css({opacity: 1})
-        }        
-    ,1000);
-
-    setTimeout(
-        function(){
-            mini_03.effect( "bounce", {times:1}, 300,'ease out bounce');
-            mini_box_03.css({opacity: 1})
-        }
-    ,1800);
-    setTimeout(
-        function(){
-            mini_04.effect( "bounce", {times:1}, 300,'ease out bounce');
-            mini_box_04.css({opacity: 1})
-        }
-    ,2600);
-    setTimeout(
-        function(){
-            mini_05.effect( "bounce", {times:1}, 300,'ease out bounce');
-            mini_box_05.css({opacity: 1})
-        }
-    ,3400);
-
-}
--- js/script.js
+++ js/script.js
@@ -1,4 +1,47 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+
+     // section_visual animation
+let mini_01 = $('.mini-01');
+let mini_box_01 = $('.mini-01 .mini-box-01');
+let mini_02 = $('.mini-02');
+let mini_box_02 = $('.mini-02 .mini-box-02');
+let mini_03 = $('.mini-03');
+let mini_box_03 = $('.mini-03 .mini-box-03');
+let mini_04 = $('.mini-04');
+let mini_box_04 = $('.mini-04 .mini-box-04');
+let mini_05 = $('.mini-05');
+let mini_box_05 = $('.mini-05 .mini-box-05');
+
+mini_01.effect( "bounce", {times:1}, 300,'ease out bounce');
+mini_box_01.css({opacity: 1})
+
+setTimeout(
+    function(){
+        mini_02.effect( "bounce", {times:1}, 300,'ease out bounce');
+        mini_box_02.css({opacity: 1})
+    }        
+,1000);
+
+setTimeout(
+    function(){
+        mini_03.effect( "bounce", {times:1}, 300,'ease out bounce');
+        mini_box_03.css({opacity: 1})
+    }
+,1800);
+setTimeout(
+    function(){
+        mini_04.effect( "bounce", {times:1}, 300,'ease out bounce');
+        mini_box_04.css({opacity: 1})
+    }
+,2600);
+setTimeout(
+    function(){
+        mini_05.effect( "bounce", {times:1}, 300,'ease out bounce');
+        mini_box_05.css({opacity: 1})
+    }
+,3400);
+
+});
 
 window.onload = function () {
 
@@ -98,21 +141,25 @@ window.onload = function () {
     });
 
 
-    //위치 확인 - 메뉴 다크모드
-    let sc_y = $(window).scrollTop();
-    console.log(sc_y);
+    // //위치 확인 - 메뉴 다크모드
+    // let sc_y = $(window).scrollTop();
+    // console.log(sc_y);
 
 
-    let pos_check = false;
+    // let pos_check = false;
 
-    if(sc_y>=930){
-        pos_check = true;
+    // if(sc_y>=930){
+    //     pos_check = true;
 
-    }else if(sc_y >= 5090){
-        pos_check = false;
-    }
+    // }else if(sc_y >= 5090){
+    //     pos_check = false;
+    // }
 
-    console.log(pos_check);
+    // console.log(pos_check);
+
+
+    
+    
 
     
 }
